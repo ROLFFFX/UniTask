3cc522cbbb1da7cfefb7fd11d0981cf6dd5dec47
EulaFRL <97711833+EulaFRL@users.noreply.github.com>
2023-09-26 18:12:26 -0400

    more on loginstyle

--- team_one_project/src/pages/LoginSignup.jsx
+++ team_one_project/src/pages/LoginSignup.jsx
@@ -13,32 +13,38 @@ const LoginSignup = () =>{
 
 
     return(
-        <div className='container'>
-            <div className='header'>
-                <div className='text'>{action}</div>
+        <div className={'bg'}>
+            <div className='container'>
+                <div className={"uppersect"}>
+                    <div className='header'>
+                        <div className='text'>{action}</div>
+                    </div>
+                    <div className='inputs'>
+                        <div className='input'>
+                            <input type="Username"/>
+                        </div>
+                        <div className='input'>
+                            <input type="Password"/>
+                        </div>
+                        <div className='input'>
+                            <input type="Email/Phone"/>
+                        </div>
+                        <div className='input'>
+                            <input type="Set Password"/>
+                        </div>
+                        <div className='input'>
+                            <input type="Confirm Password"/>
+                        </div>
+                    </div>
+                    <button className={"submit"} id={"submitSU"}>Sign Up</button>
+                    <button className={"submit"} id={"submitLG"}>Login</button>
+                </div>
+                <div className={"lowersect"}>
+                    <button className="switch" id="toLG">Already Registered?Login</button>
+                    <button className="switch" id={"toFPW"}>Forgot Password? </button>
+                    <button className="switch" id={"toSU"}>Don't have an account?Sign up</button>
+                </div>
             </div>
-            <div className='inputs'>
-                <div className='input'>
-                    <input type="Username"/>
-                </div>
-                <div className='input'>
-                    <input type="Password"/>
-                </div>
-                <div className='input'>
-                    <input type="Email/Phone"/>
-                </div>
-                <div className='input'>
-                    <input type="Set Password"/>
-                </div>
-                <div className='input'>
-                    <input type="Confirm Password"/>
-                </div>
-            </div>
-            <button className="switch" id="toLG">Already Registered?Login</button>
-            <button className="switch" id={"toFPW"}>Forgot Password? </button>
-            <button className="switch" id={"toSU"}>Don't have an account?Sign up</button>
-            <button className={"submit"} id={"submitSU"}>Sign Up</button>
-            <button className={"submit"} id={"submitLG"}>Login</button>
         </div>
     )
 }
