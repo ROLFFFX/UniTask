3cd4d2c475e0195e0dde47678780bc2eaed9c32c
Alec5 <alec.berger7@gmail.com>
2023-09-27 14:28:55 -0400

    quickc update
    qq

--- team_one_project/src/components/Links.js
+++ team_one_project/src/components/Links.js
@@ -1,11 +1,23 @@
 import "../App.css";
+<<<<<<< Updated upstream
 import UniTaskLogo from "../images/Logo.PNG";
+=======
+import UniTaskLogo_new from "../images/UniTaskLOGO.PNG"; 
+import UniTaskLogo_old from "../images/Logo_old.PNG";
+
+import LoginSignup from "../pages/LoginSignup";
+import { Link } from "react-router-dom";
+>>>>>>> Stashed changes
 
 function Links() {
   return (
     <div className="App">
       <header className="App-header">
+<<<<<<< Updated upstream
         <img src={UniTaskLogo} className="App-logo" alt="logo" />
+=======
+        <img src={UniTaskLogo_new} className="App-logo" alt="logo" />
+>>>>>>> Stashed changes
         <pre></pre>
         <a
           className="App-link"
