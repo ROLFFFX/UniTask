25d4e4571d994f17dff61d4c3d7e7ace5956d8ae
sliu <sliuwizarding@gmail.com>
2023-10-13 18:55:11 -0400

    Backend template

---
+++ unitask/src/test/java/com/cs370/unitask/UnitaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cs370.unitask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnitaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
