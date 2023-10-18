f2033421172c4a92052e6d75fb66f9e31e8e7d47
Yuxuan Shi <1051268839@qq.com>
2023-10-17 21:39:34 -0400

    Revert Encryption on Sign Up

--- team_one_project/src/components/LoginPage/LoginSignup.js
+++ team_one_project/src/components/LoginPage/LoginSignup.js
@@ -29,60 +29,54 @@ export function LoginSignup() {
   };
 
   return (
-    <ThemeProvider theme={defaultTheme}>
-      <Container component="main" maxWidth="xs">
-        <CssBaseline />
-        <Box
-          sx={{
-            marginTop: 20,
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-          }}
-        >
-          <Avatar sx={{ m: 1 }}>
-            <LockOutlinedIcon />
-          </Avatar>
-          <Typography component="h1" variant="h5">
-            Sign in
-          </Typography>
-          <Box
-            component="form"
-            onSubmit={handleSubmit}
-            noValidate
-            sx={{ mt: 1 }}
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <Box
+        sx={{
+          marginTop: 20,
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <Avatar sx={{ m: 1 }}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Sign In
+        </Typography>
+        <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            label="Email Address"
+            name="email"
+            autoComplete="email"
+            autoFocus
+          />
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Password"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+          />
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            sx={{ mt: 3, mb: 2 }}
           >
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              id="email"
-              label="Email Address"
-              name="email"
-              autoComplete="email"
-              autoFocus
-            />
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              name="password"
-              label="Password"
-              type="password"
-              id="password"
-              autoComplete="current-password"
-            />
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              sx={{ mt: 3, mb: 2 }}
-            >
-              Sign In
-            </Button>
-            <Grid container>
-              <Grid item xs>
-                {/* <Button
+            Sign In
+          </Button>
+          <Grid container>
+            <Grid item xs>
+              {/* <Button
                   type="submit"
                   fullWidth
                   variant="contained"
@@ -90,19 +84,18 @@ export function LoginSignup() {
                 >
                   Sign In
                 </Button> */}
-                <Link href="login/forgotpassword" variant="body2">
-                  {"Forgot password?"}
-                </Link>
-              </Grid>
-              <Grid item>
-                <Link href="login/signup" variant="body2">
-                  {"Don't have an account? Sign Up!"}
-                </Link>
-              </Grid>
+              <Link href="login/forgotpassword" variant="body2">
+                {"Forgot password?"}
+              </Link>
             </Grid>
-          </Box>
+            <Grid item>
+              <Link href="login/signup" variant="body2">
+                {"Don't have an account? Sign Up!"}
+              </Link>
+            </Grid>
+          </Grid>
         </Box>
-      </Container>
-    </ThemeProvider>
+      </Box>
+    </Container>
   );
 }
