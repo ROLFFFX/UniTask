1de66d9532eb8f481d62ca45075791365d17c0ab
SichenLiu111 <111249150+SichenLiu111@users.noreply.github.com>
2023-10-18 21:52:51 -0400

    Delete unitaskproject directory

--- unitaskproject/src/main/java/com/backend/unitaskproject/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.backend.unitaskproject.repository;
-
-import com.backend.unitaskproject.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    
-}
