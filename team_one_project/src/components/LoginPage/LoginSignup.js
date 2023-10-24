40a0fb9d58c71f9ed9b47d4c6345db90ee09a709
Yuxuan Shi <1051268839@qq.com>
2023-10-24 01:05:20 -0400

    Added styling on login signup

--- team_one_project/src/components/LoginPage/LoginSignup.js
+++ team_one_project/src/components/LoginPage/LoginSignup.js
@@ -1,17 +1,16 @@
-import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
-import Avatar from "@mui/material/Avatar";
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
 import Container from "@mui/material/Container";
 import CssBaseline from "@mui/material/CssBaseline";
 import Grid from "@mui/material/Grid";
 import Link from "@mui/material/Link";
-import { createTheme } from "@mui/material/styles";
 import TextField from "@mui/material/TextField";
 import Typography from "@mui/material/Typography";
+import { ThemeProvider } from "@mui/material/styles";
 import * as React from "react";
-
-const defaultTheme = createTheme();
+import theme from "./LoginStyling/theme";
+import { TopSVG } from "./LoginStyling/TopSVG";
+import { BottomSVG } from "./LoginStyling/BottomSVG";
 
 export function LoginSignup() {
   const handleSubmit = (event) => {
@@ -24,51 +23,67 @@ export function LoginSignup() {
   };
 
   return (
-    <Container component="main" maxWidth="xs">
-      <CssBaseline />
-      <Box
-        sx={{
-          marginTop: 20,
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
-        }}
-      >
-        <Typography component="h1" variant="h5">
-          Sign In
-        </Typography>
-        <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
-          <TextField
-            margin="normal"
-            required
-            fullWidth
-            id="email"
-            label="Email Address"
-            name="email"
-            autoComplete="email"
-            autoFocus
-          />
-          <TextField
-            margin="normal"
-            required
-            fullWidth
-            name="password"
-            label="Password"
-            type="password"
-            id="password"
-            autoComplete="current-password"
-          />
-          <Button
-            type="submit"
-            fullWidth
-            variant="contained"
-            sx={{ mt: 3, mb: 2 }}
-          >
+    <ThemeProvider theme={theme}>
+      <TopSVG></TopSVG>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 25,
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            width: "100%", // Set width to 100% or another desired value
+            maxWidth: "lg", // Or another desired value, or remove maxWidth
+            padding: "40px",
+            backgroundColor: "#F6F7FC",
+            borderRadius: "16px", // Adjust this value for more or less rounded corners
+            boxShadow: "0 3px 5px rgba(0, 0, 0, 0.3)", // Adjust values and color for desired shadow effect
+          }}
+          // border={4}
+        >
+          <Typography component="h1" variant="h5">
             Sign In
-          </Button>
-          <Grid container>
-            <Grid item xs>
-              {/* <Button
+          </Typography>
+          <Box
+            component="form"
+            onSubmit={handleSubmit}
+            noValidate
+            sx={{ mt: 1 }}
+          >
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email Address"
+              name="email"
+              autoComplete="email"
+              autoFocus
+              InputLabelProps={{ style: { fontSize: 14 } }}
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+              InputLabelProps={{ style: { fontSize: 14 } }}
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Sign In
+            </Button>
+            <Grid container>
+              <Grid item xs>
+                {/* <Button
                   type="submit"
                   fullWidth
                   variant="contained"
@@ -76,18 +91,20 @@ export function LoginSignup() {
                 >
                   Sign In
                 </Button> */}
-              <Link href="login/forgotpassword" variant="body2">
-                {"Forgot password?"}
-              </Link>
+                {/* <Link href="login/forgotpassword" variant="body2">
+                  {"Forgot password?"}
+                </Link> */}
+              </Grid>
+              <Grid item>
+                <Link href="login/signup" variant="body2">
+                  {"Don't have an account? Sign Up!"}
+                </Link>
+              </Grid>
             </Grid>
-            <Grid item>
-              <Link href="login/signup" variant="body2">
-                {"Don't have an account? Sign Up!"}
-              </Link>
-            </Grid>
-          </Grid>
+          </Box>
         </Box>
-      </Box>
-    </Container>
+      </Container>
+      <BottomSVG></BottomSVG>
+    </ThemeProvider>
   );
 }
