f20effc7903d137725d3bb209fc6d429921347a1
EulaFRL <97711833+EulaFRL@users.noreply.github.com>
2023-10-24 00:20:18 -0400

    Update HyperlinkDrawer.js

--- team_one_project/src/components/HyperLink/HyperlinkDrawer.js
+++ team_one_project/src/components/HyperLink/HyperlinkDrawer.js
@@ -11,28 +11,19 @@ import ListItemText from '@mui/material/ListItemText';
 import InboxIcon from '@mui/icons-material/MoveToInbox';
 import MailIcon from '@mui/icons-material/Mail';
 
-export default function TemporaryDrawer() {
-  const [state, setState] = React.useState({
-    top: false,
-    left: false,
-    bottom: false,
-    right: false,
-  });
+export default function HyperlinkDrawer() {
+  const [state, setState] = React.useState(false);
 
-  const toggleDrawer = (anchor, open) => (event) => {
-    if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
-      return;
-    }
-
-    setState({ ...state, [anchor]: open });
+  const toggleDrawer = (event) =>  {
+    setState(event);
   };
 
-  const list = (anchor) => (
+  const list = () => (
     <Box
-      sx={{ width: anchor === 'top' || anchor === 'bottom' ? 'auto' : 250 }}
+      sx={{ width: 'auto'}}
       role="presentation"
-      onClick={toggleDrawer(anchor, false)}
-      onKeyDown={toggleDrawer(anchor, false)}
+      onClick={()=>toggleDrawer(true)}
+      // onKeyDown={toggleDrawer(anchor, false)}
     >
       <List>
         {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
@@ -64,18 +55,16 @@ export default function TemporaryDrawer() {
 
   return (
     <div>
-      {['left', 'right', 'top', 'bottom'].map((anchor) => (
-        <React.Fragment key={anchor}>
-          <Button onClick={toggleDrawer(anchor, true)}>{anchor}</Button>
+        <React.Fragment>
+          <Button onClick={()=>toggleDrawer(true)}>Hyperlinks</Button>
           <Drawer
-            anchor={anchor}
-            open={state[anchor]}
-            onClose={toggleDrawer(anchor, false)}
+            anchor='top'
+            open={state}
+            onClose={()=>toggleDrawer(false)}
           >
-            {list(anchor)}
+            {list()}
           </Drawer>
         </React.Fragment>
-      ))}
     </div>
   );
 }
