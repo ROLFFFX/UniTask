818aa0dc0024fe13b04b16e8ed0b908b3a5ea773
Alec5 <alec.berger7@gmail.com>
2023-10-31 04:50:59 -0400

    Update Taskboard
    cleaned up code

--- team_one_project/src/components/SprintBoard/MainSprintBoard.js
+++ team_one_project/src/components/SprintBoard/MainSprintBoard.js
@@ -1,16 +1,15 @@
 import { Box } from "@mui/material";
-import Popper from '@mui/material/Popper';
-import React, {useState, useEffect} from "react";
+import Popper from "@mui/material/Popper";
+import React, { useState, useEffect } from "react";
 import "../../App.css";
 import add_button_favicon from "../../images/add_button_favicon.png";
 import chevron_favicon from "../../images/chevron_favicon.png";
 import circle_blue_favicon from "../../images/circle_blue_favicon.png";
 import circle_orange_favicon from "../../images/circle_orange_favicon.png";
 import "./MainSprintBoard.css";
-import {v4 as uuidv4} from "uuid";
+import { v4 as uuidv4 } from "uuid";
 
 export function MainSprintBoard() {
-
   const [AddSub, setAddSub] = useState("idle");
   const [ShowSub, setShowSub] = useState("expand");
 
@@ -24,7 +23,7 @@ export function MainSprintBoard() {
     const newlist = listSub.concat([
       {
         id: uuidv4(), //to have a stable key attribute for the item
-        description: "Subtask"
+        description: "Subtask",
       },
     ]);
     setlistSub(newlist);
@@ -37,7 +36,15 @@ export function MainSprintBoard() {
   // Simulate fetching the list of users from a database
   useEffect(() => {
     // Hard code user names for now
-    const tempUsers = ["Alec", "Daniel", "Eula", "Francis", "Rolf", "Salina", "Sichen"];
+    const tempUsers = [
+      "Alec",
+      "Daniel",
+      "Eula",
+      "Francis",
+      "Rolf",
+      "Salina",
+      "Sichen",
+    ];
     setUsers(tempUsers);
   }, []);
 
@@ -49,7 +56,6 @@ export function MainSprintBoard() {
     setAnchorEl(anchorEl ? null : event.currentTarget);
   };
 
-
   // Close popup menu and submit data
   const [teamName, setTeamName] = useState("Team 1");
   const [tasks, setTasks] = useState([]);
@@ -59,24 +65,25 @@ export function MainSprintBoard() {
   const [taskPointsInput, setTaskPointsInput] = useState(1);
 
   const closeTaskPopup = () => {
-   // if (taskNameInput) {
-        // TODO: send data to backend
-        const taskData = {
-          title: taskNameInput.valueOf(),
-          assignee: assigneeInput.valueOf(),
-          expectedCompleteTime: dueDateInput.valueOf(),
-          status: "Not Started", // TODO: get from user
-          taskPoints: taskPointsInput.valueOf(),
-          parentTaskID: null,  // TODO: set parent ID if applicable
-          numLayers: 1 // TODO: calculate layer count
-        }
-        createTask(taskData);
-        displayTask(taskData);
+    // if (taskNameInput) {
+    // TODO: send data to backend
+    const taskData = {
+      title: taskNameInput.valueOf(),
+      assignee: assigneeInput.valueOf(),
+      expectedCompleteTime: dueDateInput.valueOf(),
+      status: "Not Started", // TODO: get from user
+      taskPoints: taskPointsInput.valueOf(),
+      parentTaskID: null, // TODO: set parent ID if applicable
+      numLayers: 1, // TODO: calculate layer count
+    };
+    createTask(taskData);
+    displayTask(taskData);
 
-        // Reset input fields
-        setTaskNameInput("");
-        setTaskPointsInput(1);
-   // }
+    // Reset input fields
+    // TODO: add more fields
+    setTaskNameInput("");
+    setTaskPointsInput(1);
+    // }
   };
 
   const createTask = (taskData) => {
@@ -97,77 +104,94 @@ export function MainSprintBoard() {
   };
 
   return (
-    <Box sx={{ marginLeft: "240px" }}>
+    <Box sx={{ marginLeft: "200px" }}>
       <title>Taskboard</title>
       <div id="main">
         <div className="grid-container" id="board">
           <div className="grid-item" id="tasksHeader">
             Tasks
-              <img id="addTaskButton" aria-describedby={"createTaskMenu"} onClick={handleClick} src={add_button_favicon} alt=""></img>
+            <img
+              id="addTaskButton"
+              aria-describedby={"createTaskMenu"}
+              onClick={handleClick}
+              src={add_button_favicon}
+              alt=""
+            ></img>
           </div>
           <Popper id={"createTaskMenu"} open={open} anchorEl={anchorEl}>
             <Box className="popupContent">
               <span>Title: </span>
-              <input type='text'
-                        id='taskNameInput'
-                        value={taskNameInput}
-                        onChange={e => setTaskNameInput(e.target.value)}
+              <input
+                type="text"
+                id="taskNameInput"
+                value={taskNameInput}
+                onChange={(e) => setTaskNameInput(e.target.value)}
               ></input>
-              <br></br><br></br>
+              <br></br>
+              <br></br>
               <span>Assigned to: </span>
-              <select name="assigneeInput" 
-                      id="assigneeInput"
-                      value={assigneeInput}
-                      onChange={e => setAssigneeInput(e.target.value)}
-              ><option value="">Select User</option>
+              <select
+                name="assigneeInput"
+                id="assigneeInput"
+                value={assigneeInput}
+                onChange={(e) => setAssigneeInput(e.target.value)}
+              >
+                <option value="">Select User</option>
                 {users.map((user, index) => (
                   <option key={index} value={user}>
                     {user}
                   </option>
                 ))}
               </select>
-              <br></br><br></br>
+              <br></br>
+              <br></br>
               <span>Due date: </span>
-              <input type='date'
-                        id='dueDateInput'
-                        value={dueDateInput}
-                        onChange={e => setDueDateInput(e.target.value)}
+              <input
+                type="date"
+                id="dueDateInput"
+                value={dueDateInput}
+                onChange={(e) => setDueDateInput(e.target.value)}
               ></input>
-              <br></br><br></br>
+              <br></br>
+              <br></br>
               <span>Task Points: </span>
-              <input type='number'
-                        id='taskPointsInput'
-                        value={taskPointsInput}
-                        onChange={e => setTaskPointsInput(e.target.value)}
+              <input
+                type="number"
+                id="taskPointsInput"
+                value={taskPointsInput}
+                onChange={(e) => setTaskPointsInput(e.target.value)}
               ></input>
-              
-              <button id="closeTaskPopupButton" onClick={closeTaskPopup}>Submit</button>
+
+              <button id="closeTaskPopupButton" onClick={closeTaskPopup}>
+                Submit
+              </button>
             </Box>
           </Popper>
           <div className="grid-item" id="tasks">
-            <ul id={"taskList"}>
+          <ul id={"taskList"}>
               {listTask.map((task) => (
-                <li
-                    className={"task"}
-                    key={task.id}
-                >
+                <li className={"task"} key={task.id}>
                   <div className="taskLabel">
                     <img src={circle_orange_favicon} alt=""></img>
                     {task.title}
-                    <button id="showSubtaskButton" onClick={() => setShowSub(ShowSub==="collapse"?
-                                                                                  ("expand")
-                                                                                  :("collapse") )}>
+                    <button 
+                      id="showSubtaskButton" 
+                      onClick={() => 
+                        setShowSub(ShowSub==="collapse"? ("expand") :("collapse") )
+                      }
+                    >
                       <img
                         className="showSubtaskButtonImg"
                         src={chevron_favicon}
                         alt=""
                       ></img>
                     </button>
-                    <button id="addSubtaskButton"
-                            onClick={() => {
-                                        addSubtaskButton();
-                                        setAddSub("activated");
-                                      }}
+                    <button 
+                      id="addSubtaskButton"
+                      onClick={() => {
+                        addSubtaskButton();
+                        setAddSub("activated");
+                      }}
                     >
                       <img
                         className="addSubtaskButtonImg"
