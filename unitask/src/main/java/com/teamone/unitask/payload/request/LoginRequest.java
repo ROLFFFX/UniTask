505bf7ce4481293a27e0333ac0c628624a828608
sliu <sliuwizarding@gmail.com>
2023-10-31 18:41:49 -0400

    Switch Login param from username to email

--- unitask/src/main/java/com/teamone/unitask/payload/request/LoginRequest.java
+++ unitask/src/main/java/com/teamone/unitask/payload/request/LoginRequest.java
@@ -6,16 +6,25 @@ import javax.validation.constraints.NotBlank;
 public class LoginRequest {
 
     @NotBlank
-    private String username;
+    private String email;
     @NotBlank
     private String password;
 
-    public String getUsername() {
-        return username;
+//    public String getUsername() {
+//        return username;
+//    }
+//
+//    public void setUsername(String username) {
+//        this.username = username;
+//    }
+
+
+    public String getEmail() {
+        return email;
     }
 
-    public void setUsername(String username) {
-        this.username = username;
+    public void setEmail(String email) {
+        this.email = email;
     }
 
     public String getPassword() {
