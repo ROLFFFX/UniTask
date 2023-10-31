8a106f1bac97ee29ac3d32377047318224c380e6
EulaFRL <97711833+EulaFRL@users.noreply.github.com>
2023-10-31 13:50:21 -0400

    Update TopNavBar.js send/update/delete/get data waiting for test

--- team_one_project/src/components/Utilities/TopNavBar.js
+++ team_one_project/src/components/Utilities/TopNavBar.js
@@ -33,22 +33,75 @@ import axios from "axios";
 
 export function TopAppBar() {
 
-  //const userlinks = {user.hyperlinks? user.hyperlinks :[]}; //depends on the structure of the object, it should be a list of hyperlinks
-  const [linksList, setLinksList] = useState([]); //TODO: change to userlinks
-  //TODO
-  const fetchLinksList=async()=>{
-    await axios.get("")//接口 url
-        .then(response => {
-          setLinksList(response.data); // depends on the structure of the object, this is supposed to be a list of hyperlinks
-        })
-        .catch(error => {
-          console.error('Error fetching data:', error);
-        });
+
+
+  const [linksList, setLinksList] = useState([]);
+  async function getLinksList() {
+    try{
+      const response = await axios.get('endpoint URL', {
+        params: {
+          //to be filled if any
+        }
+      });
+      console.log(response.data);
+      setLinksList(response.data);
+    }
+    catch(error){
+      console.error("Error getting list", error);
+    }
   }
 
   useEffect(() => {
-        fetchLinksList()
+        getLinksList()
   }, []);
+
+  const updateLinksList = async(e) => {
+    e.preventDefault();
+    console.log(linksList);
+    try{
+      const response = await axios.put("endpoint URL", linksList, {
+            params: {
+              //to be filled if any
+            },
+            headers: { "Content-Type": "application/json" },
+          });
+    } catch (error){
+      console.error("Error updating list:", error);
+    }
+  }
+
+
+  const deleteLinksList = async(e) => {
+    try{
+      const response = await axios.delete("endpoint URL", {
+        params: {
+          //to be filled if any
+        }
+      })
+    }
+    catch(error){
+      console.error("Error deleting list", error);
+    }
+  }
+
+  const submitLinksList = async (e) => {
+    e.preventDefault();
+    console.log(linksList);
+    try {
+      const response = await axios.post(
+          "endpoint URL",
+          linksList,
+          {
+            params: {
+              //to be filled if any
+            },
+            headers: { "Content-Type": "application/json" },
+          }
+      );
+    } catch (error) {
+      console.error("Error submitting list", error);
+    }
+  };
 
   const [anchorElNav, setAnchorElNav] = React.useState(null);
   const [anchorElUser, setAnchorElUser] = React.useState(null);
@@ -61,32 +114,6 @@ export function TopAppBar() {
   const [itAction, setitAction] = useState("Static"); //actions on change/remove list items
   const [linkName, setLinkName] = useState("");
   const [link, setLink] = useState("");
-
-  //TODO: update
-  const updateLinksList=async(linksList)=>{
-    await axios.post("", linksList)//接口 url
-          .then(promise => {
-
-          })
-          .catch(error => {
-            console.error('Error posting data:', error);
-          });
-  }
-
-  //TODO: delete
-
-
-  const submitLinksList = async (e) => {
-    e.preventDefault();
-    console.log(linksList);
-    try {
-      await axios.post("", linksList, {
-        headers: { "Content-Type": "application/json" },
-      });
-    } catch (error) {
-      console.error("Error Caught on Sign Up: ", error);
-    }
-  };
 
   const handleOpenNavMenu = (event) => {
     setAnchorElNav(event.currentTarget);
@@ -157,7 +184,7 @@ export function TopAppBar() {
     const newlist = linksList.filter((userlink) => userlink.id !== id);
     setLinksList(newlist);
     if(linksList===[]){//if linksList after remove is empty
-        submitLinksList();
+        deleteLinksList();
     }else{
         updateLinksList();
     }
