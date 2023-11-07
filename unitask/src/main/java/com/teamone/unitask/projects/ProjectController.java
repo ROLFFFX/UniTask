cd209b88aca20a0605bb89fbb4f541e5a69968ee
sliu <sliuwizarding@gmail.com>
2023-11-06 20:00:53 -0500

    Typo fixed

--- unitask/src/main/java/com/teamone/unitask/projects/ProjectController.java
+++ unitask/src/main/java/com/teamone/unitask/projects/ProjectController.java
@@ -14,7 +14,7 @@ import java.util.List;
 
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
-@RequestMapping("/prjects")
+@RequestMapping("/projects")
 public class ProjectController {
 
     @Autowired
