3ee5207e1de1e90444a3c51f10071124be7f152a
Alec5 <alec.berger7@gmail.com>
2023-11-07 19:12:02 -0500

    update sprintboard

---
+++ team_one_project/src/components/SprintBoard/MainSprintBoard.js
@@ -0,0 +1,238 @@
+import { Box } from "@mui/material";
+import Task from './Task';
+import Popper from "@mui/material/Popper";
+import React, { useState, useEffect } from "react";
+import "../../App.css";
+import add_button_favicon from "../../images/add_button_favicon.png";
+import chevron_favicon from "../../images/chevron_favicon.png";
+import circle_blue_favicon from "../../images/circle_blue_favicon.png";
+import circle_orange_favicon from "../../images/circle_orange_favicon.png";
+import "./MainSprintBoard.css";
+import { v4 as uuidv4 } from "uuid";
+
+export function MainSprintBoard() {
+
+  // List of users (for assignee list)
+  const [users, setUsers] = useState([]);
+  //const [selectedUser, setSelectedUser] = useState('');
+
+  // Simulate fetching the list of users from a database
+  useEffect(() => {
+    // Hard code user names for now
+    const tempUsers = [
+      "Alec",
+      "Daniel",
+      "Eula",
+      "Francis",
+      "Rolf",
+      "Salina",
+      "Sichen",
+    ];
+    setUsers(tempUsers);
+  }, []);
+
+  // From MUI Popper.js tutorial:
+  const [anchorEl, setAnchorEl] = useState(null);
+  const open = Boolean(anchorEl);
+
+  //const [teamName, setTeamName] = useState("Team 1");
+  const [tasks, setTasks] = useState([]);
+  const [taskNameInput, setTaskNameInput] = useState("");
+  const [assigneeInput, setAssigneeInput] = useState("");
+  const [dueDateInput, setDueDateInput] = useState("");
+  const [taskPointsInput, setTaskPointsInput] = useState(1);
+
+  // Show task creation popup menu
+  const openTaskPopup = (event) => {
+    setAnchorEl(anchorEl ? null : event.currentTarget);
+  };
+
+  // Close task popup menu and submit data
+  const closeTaskPopup = () => {
+    setAnchorEl(null);  // Close popup window
+    // if (taskNameInput) {
+    // TODO: send data to backend
+    const taskData = {
+      title: taskNameInput.valueOf(),
+      assignee: assigneeInput.valueOf(),
+      expectedCompleteTime: dueDateInput.valueOf(),
+      status: "Not Started", // TODO: get from user
+      taskPoints: taskPointsInput.valueOf(),
+      //parentTaskID: null, // TODO: set parent ID if applicable
+      //numLayers: 1, // TODO: calculate layer count
+      subtaskList: []
+    };
+    createTask(taskData);
+
+    // Reset input fields
+    // TODO: add more fields
+    setTaskNameInput("");
+    setTaskPointsInput(1);
+    // }
+  };
+
+  const createTask = (taskData) => {
+    // Add task into tasks array
+    setTasks([...tasks, taskData]);
+    console.log(tasks);   // Task list is always out of sync, need useEffect
+    // TODO: insert data into database
+  };
+
+
+  // Handle drag and drop
+  // TODO: Don't hard code column names, instead select all columns via class
+  // (sloppy temp solution implemented because don't know how to add multiple classes in React)
+  const tasksColumn = document.getElementById("tasksColumn");
+  const todoColumn = document.getElementById("todoColumn");
+  const doingColumn = document.getElementById("doingColumn");
+  const doneColumn = document.getElementById("doneColumn");
+  const columns = [tasksColumn, todoColumn, doingColumn, doneColumn]
+
+/*
+  columns.forEach((column) => {
+    if (column) {
+      column.addEventListener("dragover", (e) => {
+        e.preventDefault();
+        const currentTask = document.querySelector(".dragging");
+        column.appendChild(currentTask);
+      });
+    }
+  });
+*/
+
+  const onDragOver = (e) => {
+    e.preventDefault(); // Allow the drop
+  };
+
+  const onDrop = (e, targetContainerId) => {
+    e.preventDefault();
+    /*
+    const jsonDataString = e.dataTransfer.getData('application/json'); // Retrieve the JSON string
+    const data = JSON.parse(jsonDataString); // Parse the JSON string into an object
+    const sourceContainerId = e.dataTransfer.getData('containerId'); // Retrieve the source container identifier
+    // Source container id isn't working properly but it might not matter?
+    console.log(`Dropped into container ${targetContainerId} from ${sourceContainerId}`); // Can identify the target column
+    //console.log(data);  // Can receive all the task data (note: this is a copy of the JSON data, not a reference to the original)
+    */
+    const currentTask = document.querySelector(".dragging");
+    document.getElementById(targetContainerId).appendChild(currentTask);
+    
+  };
+
+
+  return (
+    <Box sx={{ marginLeft: "200px" }}>
+      <title>Taskboard</title>
+      <div id="main">
+        <div className="grid-container" id="board">
+          <div className="grid-item" id="tasksHeader">
+            Tasks
+            <img
+              id="addTaskButton"
+              aria-describedby={"createTaskMenu"}
+              onClick={openTaskPopup}
+              src={add_button_favicon}
+              alt=""
+            ></img>
+          </div>
+          <Popper id={"createTaskMenu"} open={open} anchorEl={anchorEl}>
+            <Box className="popupContent">
+              <span>Title: </span>
+              <input
+                type="text"
+                id="taskNameInput"
+                value={taskNameInput}
+                onChange={(e) => setTaskNameInput(e.target.value)}
+              ></input>
+              <br></br>
+              <br></br>
+              <span>Assigned to: </span>
+              <select
+                name="assigneeInput"
+                id="assigneeInput"
+                value={assigneeInput}
+                onChange={(e) => setAssigneeInput(e.target.value)}
+              >
+                <option value="Unassigned">Unassigned</option>
+                {users.map((user, index) => (
+                  <option key={index} value={user}>
+                    {user}
+                  </option>
+                ))}
+              </select>
+              <br></br>
+              <br></br>
+              <span>Due date: </span>
+              <input
+                type="date"
+                id="dueDateInput"
+                value={dueDateInput}
+                onChange={(e) => setDueDateInput(e.target.value)}
+              ></input>
+              <br></br>
+              <br></br>
+              <span>Task Points: </span>
+              <input
+                type="number"
+                id="taskPointsInput"
+                value={taskPointsInput}
+                onChange={(e) => setTaskPointsInput(e.target.value)}
+              ></input>
+
+              <button id="closeTaskPopupButton" onClick={closeTaskPopup}>
+                Submit
+              </button>
+            </Box>
+          </Popper>
+          <div className="grid-item" id="tasksColumn">
+            {tasks.map((task, index) => (
+              <Task key={index} taskData={task} />
+            ))}
+          </div>
+          <div className="grid-item" id="todoHeader">
+            TO DO
+          </div>
+          <div className="grid-item" id="todoColumn" onDragOver={onDragOver} onDrop={(e) => onDrop(e, 'todoColumn')}></div>
+          <div className="grid-item" id="doingHeader">
+            DOING
+          </div>
+          <div className="grid-item" id="doingColumn" onDragOver={onDragOver} onDrop={(e) => onDrop(e, 'doingColumn')}></div>
+          <div className="grid-item" id="doneHeader">
+            DONE
+          </div>
+          <div className="grid-item" id="doneColumn" onDragOver={onDragOver} onDrop={(e) => onDrop(e, 'doneColumn')}></div>
+        </div>
+      </div>
+    </Box>
+  );
+
+
+  /*
+    STUFF IN RETURN
+            {tasks.map(task => (
+                <li className='task' key={task.taskName}>
+                </li>
+            ))}
+            <div id='createTaskMenu' className='popup'>
+                <div className='popupContent'>
+                    <input
+                        type='text'
+                        id='taskNameInput'
+                        value={taskNameInput}
+                        onChange={e => setTaskNameInput(e.target.value)}
+                    />
+                    <button id='closeTaskPopupButton' onClick={closeTaskPopup}>
+                        Submit
+                    </button>
+                </div>
+            </div>
+    */
+
+  /*<div id="createTaskMenu" className="popup">
+                    <div className="popupContent">
+                        <input type="text" id="taskNameInput"></input>
+                        <button id="closeTaskPopupButton" onClick="closeTaskPopup()">Submit</button>
+                    </div>
+                </div> */
+
+}
---
+++ team_one_project/src/components/SprintBoard/Task.js
@@ -0,0 +1,117 @@
+import React, { useState, useRef } from 'react';
+import chevron_favicon from "../../images/chevron_favicon.png";
+import circle_blue_favicon from "../../images/circle_blue_favicon.png";
+import circle_orange_favicon from "../../images/circle_orange_favicon.png";
+import add_button_favicon from "../../images/add_button_favicon.png";
+
+function Task({ taskData }) {
+  const [subtasks, setSubtasks] = useState([]);
+  const [newSubtask, setNewSubtask] = useState('');
+  const [isAddingSubtask, setIsAddingSubtask] = useState(false);
+
+  const [AddSub, setAddSub] = useState("idle");
+  const [ShowSub, setShowSub] = useState("expand");
+
+  // Used to reference the current task being dragged
+  const elementRef = useRef(null);
+
+  const addSubtask = () => {
+    setSubtasks([...subtasks, newSubtask]);
+    taskData.subtaskList = subtasks;
+    console.log(taskData);  // TODO: Fix bug where the last subtask is not visible in list
+    setNewSubtask('');
+    setIsAddingSubtask(false);
+  };
+
+  // Add dragging capability
+  // When item is dragged, give it the dragging class
+  const drag = (e, containerId) => {
+    if (elementRef.current) {
+      elementRef.current.classList.add('dragging');
+      
+      //e.dataTransfer.setData('application/json', JSON.stringify(taskData)); // Set data to enable drag
+      //e.dataTransfer.setData('containerId', containerId); // Send the id of the container
+    }
+  }
+
+  const endDrag = () => {
+    if (elementRef.current) {
+      elementRef.current.classList.remove('dragging');
+      taskData.status = elementRef.current.parentElement.id;
+      console.log(taskData);
+    }
+  }
+
+
+  return (
+    <div 
+      className={"task"}
+      key={taskData.id}
+      draggable="true"
+      ref={elementRef}
+      onDragStart={(containerId) => drag(containerId)}
+      onDragEnd={endDrag}
+    >
+        <div className="taskLabel">
+        <img src={circle_orange_favicon} alt=""></img>
+        {taskData.title}
+        <button 
+            className="showSubtaskButton" 
+            onClick={() => 
+            setShowSub(ShowSub==="collapse"? ("expand") :("collapse") )
+            }
+        >
+            <img
+            className="showSubtaskButtonImg"
+            src={chevron_favicon}
+            alt=""
+            ></img>
+        </button>
+        <button 
+            className="newSubtaskButton"
+            onClick={() => setIsAddingSubtask(true)}
+        >
+            <img
+                className="newSubtaskButtonImg"
+                src={add_button_favicon}
+                alt={""}
+            ></img>
+        </button>
+        
+        </div>
+        {ShowSub === "expand" ?
+            (<ul className={"subtaskList"}>
+            {subtasks.map((subtask, index) => (
+                <li
+                    className={"subtask"}
+                    key={index}
+                >
+                <input type="checkbox"/>
+                {subtask}
+                </li>
+            ))}
+            </ul>)
+            : null
+        }
+        {isAddingSubtask ? (
+        <div>
+          <input
+            type="text"
+            value={newSubtask}
+            onChange={(e) => setNewSubtask(e.target.value)}
+          />
+          <button 
+            className="addSubtaskButton"
+            onClick={addSubtask}
+          >
+            Add Subtask
+          </button>
+        </div>
+      ) : (
+        null
+      )}
+    </div>    
+  );
+}
+
+export default Task;
