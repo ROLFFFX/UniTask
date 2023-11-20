9923fff096d24137c251a4aca493b9fe04607afb
Yuxuan Shi <1051268839@qq.com>
2023-11-20 11:44:38 -0500

    POST & GET method implemented

--- team_one_project/src/components/SprintBoard/MainSprintBoard.js
+++ team_one_project/src/components/SprintBoard/MainSprintBoard.js
@@ -35,32 +35,29 @@ const dummyTaskfromBackend = [
     duedate: "2023-12-01",
     taskPoints: "5",
   }, //userName is assignee
-  {
-    taskID: 3,
-    taskName: "In progress task",
-    userName: "Sichen",
-    status: "Doing",
-    duedate: "2023-12-01",
-    taskPoints: "3",
-  }, //userName is assignee
-  {
-    taskID: 4,
-    taskName: "Done task",
-    userName: "Rolf",
-    status: "Done",
-    duedate: "2023-12-01",
-    taskPoints: "1",
-  }, //userName is assignee
+  // {
+  //   taskID: 3,
+  //   taskName: "In progress task",
+  //   userName: "Sichen",
+  //   status: "Doing",
+  //   duedate: "2023-12-01",
+  //   taskPoints: "3",
+  // }, //userName is assignee
+  // {
+  //   taskID: 4,
+  //   taskName: "Done task",
+  //   userName: "Rolf",
+  //   status: "Done",
+  //   duedate: "2023-12-01",
+  //   taskPoints: "1",
+  // }, //userName is assignee
 ];
 
 export function MainSprintBoard() {
-  /* Helper functions for request cycles */
   const { auth } = useAuth();
   const [backdropOpen, setBackdropOpen] = useState(false); //loading page
-  const refreshTeamMembers = () => {
-    //refetch team member, specifically used after success invitation
-    fetchTeamMembers(); // Re-fetch team members
-  };
+
+  /* Helper functions for request cycles */
   /* First Step: Getting project members */
   const [users, setUsers] = useState([]);
   const projectTitle = auth.selectedWorkspace;
@@ -86,47 +83,107 @@ export function MainSprintBoard() {
     }
   };
   useEffect(() => {
-    // This useEffect hook will refetch the team members. The dependency list is not finalized.
+    // This useEffect hook will refetch the team members for initial rendering. The dependency list is not finalized.
     fetchTeamMembers();
   }, []);
+  const refreshTeamMembers = () => {
+    //refetch team member, specifically used after success invitation
+    fetchTeamMembers(); // Re-fetch team members
+  };
   /* First Step: End of Getting project members */
 
   // From MUI Popper.js tutorial:
   const [anchorEl, setAnchorEl] = useState(null);
   const open = Boolean(anchorEl);
 
-  //const [teamName, setTeamName] = useState("Team 1");
+  const [teamName, setTeamName] = useState(projectTitle); //teamName is initialized as the projectTitle initialized and updated at global auth state.
   const [tasks, setTasks] = useState([]);
   const [taskNameInput, setTaskNameInput] = useState("");
   const [assigneeInput, setAssigneeInput] = useState("");
   const [dueDateInput, setDueDateInput] = useState("");
   const [taskPointsInput, setTaskPointsInput] = useState(1);
 
-  // Reformat task data from backend into taskData format
-  const unpackTaskData = (backendTasks) => {
-    setTasks((prevTasks) => [
-      ...prevTasks,
-      ...backendTasks.reduce((uniqueTasks, task) => {
-        // Check if the taskID already exists in the state
-        if (!prevTasks.some((prevTask) => prevTask.taskID === task.taskID)) {
-          uniqueTasks.push({
-            taskID: task.taskID, // Assuming taskID is a unique identifier
-            title: task.taskName,
-            assignee: task.userName,
-            dueDate: task.duedate,
-            status: task.status,
-            taskPoints: task.taskPoints,
-            subtaskList: [],
-          });
+  const [unformattedTasks, setUnformattedTasks] = useState();
+  const fetchAllTasks = async () => {
+    try {
+      const response = await axios.get(
+        `${ENDPOINT_URL}tasks/getAllTask?projectTitle=${projectTitle}`,
+        {
+          headers: {
+            Authorization: `Bearer ${auth.user.userJWT}`,
+          },
         }
-        return uniqueTasks;
-      }, []),
-    ]);
+      );
+      // console.log(response); //this response contains all tasks
+      setUnformattedTasks(response.data);
+      console.log("Done fetching tasks successfully!");
+    } catch (error) {
+      console.error("Error fetching tasks: ", error);
+    }
+  };
+
+  // Reformat task data from backend into taskData format
+  // const unpackTaskData = (backendTasks) => {
+  //   // Process each list (task with its subtasks)
+  //   const reformattedTasks = backendTasks.map((taskList) => {
+  //     const [mainTask, ...subTasks] = taskList;
+  //     return {
+  //       taskID: mainTask.taskId,
+  //       title: mainTask.title,
+  //       assignee: "Yuxuan Shi", // to be adjusted mainTask.userName
+  //       dueDate: mainTask.expectedCompleteTime,
+  //       status: mainTask.status,
+  //       taskPoints: mainTask.taskPoints,
+  //       subtaskList: subTasks.map((subTask) => ({
+  //         taskID: subTask.taskId,
+  //         title: subTask.title,
+  //         assignee: subTask.userName, // todo
+  //         dueDate: subTask.expectedCompleteTime,
+  //         status: subTask.status,
+  //         taskPoints: subTask.taskPoints,
+  //         // Add more fields if necessary
+  //       })),
+  //     };
+  //   });
+
+  //   setTasks(reformattedTasks);
+  // };
+  const unpackTaskData = (backendTasks) => {
+    const reformattedTasks = backendTasks.flatMap((taskList) => {
+      const [mainTask, ...subTasks] = taskList;
+
+      return {
+        taskID: mainTask.taskId,
+        title: mainTask.title,
+        assignee: "Yuxuan Shi", // to be adjusted, mainTask.userName
+        dueDate: mainTask.expectedCompleteTime,
+        status: mainTask.status,
+        taskPoints: mainTask.taskPoints,
+        subtaskList: subTasks.map((subTask) => ({
+          taskID: subTask.taskId,
+          title: subTask.title,
+          assignee: subTask.userName, // Adjust based on your data
+          dueDate: subTask.expectedCompleteTime,
+          status: subTask.status,
+          taskPoints: subTask.taskPoints,
+        })),
+      };
+    });
+
+    console.log(reformattedTasks); // Debugging line
+    setTasks(reformattedTasks);
   };
 
   useEffect(() => {
-    unpackTaskData(dummyTaskfromBackend);
+    fetchAllTasks();
+    // GET all tasks from backend
   }, []);
+
+  useEffect(() => {
+    if (unformattedTasks) {
+      unpackTaskData(unformattedTasks);
+    }
+  }, [unformattedTasks]);
 
   // Show task creation popup menu
   const openTaskPopup = (event) => {
@@ -148,6 +205,7 @@ export function MainSprintBoard() {
       //numLayers: 1, // TODO: calculate layer count
       subtaskList: [],
     };
+
     createTask(taskData);
 
     // Reset input fields
@@ -158,11 +216,48 @@ export function MainSprintBoard() {
     // }
   };
 
+  //axios.post new Task
   const createTask = (taskData) => {
-    // Add the new task to the tasks array
-    setTasks([...tasks, taskData]);
-    console.log(tasks); // Fix bug where tasks list is always one behind
+    // // Add the new task to the tasks array
+    // setTasks([...tasks, taskData]);    Task State should be handled by the Get Method
+    // console.log("This is the task info before inserting into db: ");
+    // console.log(taskData); // Fix bug where tasks list is always one behind
     // TODO: insert data into database
+
+    // Step 1: Format the request body to be sent
+    // convert date object
+    let dateObject = new Date(taskData.dueDate);
+    let isoDateString = dateObject.toISOString();
+    const requestBody = {
+      title: taskData.title,
+      status: taskData.status,
+      expectedCompleteTime: isoDateString,
+      taskPoints: taskData.taskPoints,
+    };
+    const assignee = taskData.assignee;
+    const taskId = -1; //create Task only create parent task, hence taskId is null. **should be long
+    const username = taskData.assignee;
+    const jwt = auth.user.userJWT;
+    // console.log(requestBody);
+    //start post request to create new task
+    axios
+      .post(
+        `${ENDPOINT_URL}tasks/createTask?taskId=${taskId}&projectTitle=${projectTitle}&username=${assignee}`,
+        requestBody,
+        {
+          headers: {
+            Authorization: `Bearer ${auth.user.userJWT}`,
+          },
+        }
+      )
+      .then((response) => {
+        // refetch all tasks and rerender page
+        fetchAllTasks();
+        console.log("Task created:", response.data); // @todo: response.data contains the created task info.
+      })
+      .catch((error) => {
+        console.error("Error creating task:", error);
+      });
   };
 
   const onDragOver = (e) => {
@@ -282,7 +377,7 @@ export function MainSprintBoard() {
                 </button>
               </Box>
             </Popper>
-            <div
+            {/* <div
               className="grid-item"
               id="tasksColumn"
               onDragOver={onDragOver}
@@ -293,6 +388,25 @@ export function MainSprintBoard() {
                 .map((task) => (
                   <Task
                     key={task.id}
+                    taskData={task}
+                    onDelete={deleteTask}
+                    onEdit={editTask}
+                  />
+                ))}
+            </div> */}
+            <div
+              className="grid-item"
+              id="tasksColumn"
+              onDragOver={onDragOver}
+              onDrop={(e) => onDrop(e, "tasksColumn")}
+            >
+              {tasks
+                .filter((task) =>
+                  task.status.toLowerCase().includes("not started")
+                )
+                .map((task) => (
+                  <Task
+                    key={task.taskID} // Make sure this is the correct unique identifier
                     taskData={task}
                     onDelete={deleteTask}
                     onEdit={editTask}
@@ -309,7 +423,7 @@ export function MainSprintBoard() {
               onDrop={(e) => onDrop(e, "todoColumn")}
             >
               {tasks
-                .filter((task) => task.status.includes("Todo"))
+                .filter((task) => task.status.toLowerCase().includes("todo"))
                 .map((task) => (
                   <Task
                     key={task.id}
@@ -329,7 +443,7 @@ export function MainSprintBoard() {
               onDrop={(e) => onDrop(e, "doingColumn")}
             >
               {tasks
-                .filter((task) => task.status.includes("Doing"))
+                .filter((task) => task.status.toLowerCase().includes("doing"))
                 .map((task) => (
                   <Task
                     key={task.id}
@@ -349,7 +463,7 @@ export function MainSprintBoard() {
               onDrop={(e) => onDrop(e, "doneColumn")}
             >
               {tasks
-                .filter((task) => task.status.includes("Done"))
+                .filter((task) => task.status.toLowerCase().includes("done"))
                 .map((task) => (
                   <Task
                     key={task.id}
