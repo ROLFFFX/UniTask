421fc8beb01f1a4bdbae6f457e85b776342db8b4
EulaFRL <97711833+EulaFRL@users.noreply.github.com>
2023-11-22 04:11:40 -0500

    Merge Previous Changes

--- unitask/src/main/java/com/teamone/unitask/meetings/MeetingService.java
+++ unitask/src/main/java/com/teamone/unitask/meetings/MeetingService.java
@@ -1,7 +1,53 @@
 package com.teamone.unitask.meetings;
 
+import com.teamone.unitask.projects.Project;
+import com.teamone.unitask.projects.ProjectRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class MeetingService {
+    private final MeetingRepository meetingRepository;
+    private final ProjectRepository projectRepository;
+    @Autowired
+    public MeetingService(MeetingRepository meetingRepository,
+                          ProjectRepository projectRepository)
+    {
+        this.projectRepository=projectRepository;
+        this.meetingRepository=meetingRepository;
+    }
+
+    //get the whole list of selected meetings of the project
+    public List<Meeting> get(Long project_id){
+        List<Meeting> meetings = new ArrayList(projectRepository.findByProjectId(project_id).getMeetings());
+        return meetings;
+    }
+
+    //save new meeting to the table, at the same time to the meetings list of the project
+    public Meeting save(Long project_id, String title, LocalDateTime startTime, LocalDateTime endTime){
+        Meeting new_meeting = new Meeting(title, startTime, endTime);
+        Project thisProj = projectRepository.findByProjectId(project_id);
+        new_meeting.setProjectId(thisProj);
+        meetingRepository.save(new_meeting);
+        return new_meeting;
+    }
 }
+
+
+//    //getAll for test
+//    public List<Meeting> getAll(@RequestBody Meeting meeting){
+//        List<Meeting> meetings = new ArrayList<Meeting>();
+//        meetings = meetingRepository.findAll();
+//        return meetings;
+//    }
+
+//    //get the whole list of selected meetings of the project
+//    public List<Meeting> get(Project project_id){
+//        List<Meeting> meetings = new ArrayList<Meeting>();
+//        meetingRepository.findByProjectId(project_id).forEach(meetings::add);
+//        return meetings;
+//    }
