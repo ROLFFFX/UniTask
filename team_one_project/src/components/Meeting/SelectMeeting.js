80a92c7d2ae65561e037818a29311c0ff4718365
EulaFRL <97711833+EulaFRL@users.noreply.github.com>
2023-11-25 14:40:29 -0500

    meeting pages overflow

--- team_one_project/src/components/Meeting/SelectMeeting.js
+++ team_one_project/src/components/Meeting/SelectMeeting.js
@@ -5,8 +5,11 @@ import { SelectMeetingContent } from "./SelectMeetingContent";
 
 export function SelectMeeting() {
   return (
-    <Box style={{ marginLeft: "240px" }}>
-      <Grid container spacing={2}>
+    <Box style={{ marginLeft: "240px",
+                overflowY: "auto", // Adds vertical scrollbar when needed
+                maxHeight: "calc(100vh - 200px)", // Adjust the 100px to account for headers/footers
+    }}>
+      <Grid>
         <Grid item xs={12}>
           <SelectMeetingContent></SelectMeetingContent>
         </Grid>
