a4bcfc2a50a12429b7867a2e2d2e772eb4725812
Yuxuan Shi <1051268839@qq.com>
2023-12-02 21:29:37 -0500

    Adding Comments

--- team_one_project/src/index.js
+++ team_one_project/src/index.js
@@ -1,3 +1,10 @@
+/**
+ * @fileoverview Main entry point for UniTask Front End.
+ * This file sets up the root React component and wraps it with necessary providers
+ * and routing components. It includes the setup for CookiesProvider, BrowserRouter,
+ * and AuthProvider to manage cookies, routing, and authentication contexts.
+ */
+
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
@@ -6,7 +13,15 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { AuthProvider } from "./context/AuthProvider";
 import { CookiesProvider } from "react-cookie";
 
+// root DOM element. essentially where the entire project lives
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
+/**
+ * CookiesProvider: provide cookie management throughout app, specifically used to store globa auth state.
+ * BrowserRouter: React Router set up.
+ * AuthProvider: auth state context.
+ *
+ **/
 root.render(
   // <React.StrictMode>
   <CookiesProvider>
