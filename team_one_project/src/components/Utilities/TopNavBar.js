a9d93b300f384779323e0cf425865f40b6f27826
EulaFRL <97711833+EulaFRL@users.noreply.github.com>
2023-12-04 17:17:27 -0500

    TopNavBar Documentation

--- team_one_project/src/components/Utilities/TopNavBar.js
+++ team_one_project/src/components/Utilities/TopNavBar.js
@@ -1,7 +1,18 @@
 /**
- * @fileoverview This file includes the TopAppBar component, which serves as the primary navigation bar
+ * @fileoverview
+ * This file includes the TopAppBar component, which serves as the primary navigation bar
  * for a project management application. It has a LOGO displayed at left, and three functional buttons
  * at right: a hyperlink functionality, a log-out button, and a change workspace button.
+ *
+ * Components:
+ * - LogOutButton: A functional component that renders a log-out button with tooltip.
+ * - ChangeWorkspaceButton: A functional component that renders a change workspace button with tooltip.
+ * - Hyperlinks functionality: Including a button to toggle a drawer that contains a dynamic list of hyperlinks that can be added, edited, and deleted by the user.
+ *
+ * Dependencies:
+ * - React and Material UI libraries are primary dependencies.
+ * - Axios is used for API calls.
+ * - The component also uses custom hooks and assets like `useAuth` and `UniTaskLogo_new`.
  */
 
 import AddLinkIcon from "@mui/icons-material/AddLink";
@@ -36,6 +47,18 @@ import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import useAuth from "../../hooks/useAuth";
 
+/**
+ * LogOutButton - Functional component to render a log-out button.
+ *
+ * This component creates a log-out button with an icon. When clicked, it triggers the logout
+ * function from the authentication context to log the user out of the application. It also displays
+ * a tooltip with the text "Log Out..." on hover.
+ *
+ * Utilizes Material UI's IconButton, Tooltip, and Typography components to create the button with
+ * a styled tooltip. The LogoutIcon from Material UI icons is used for the button's icon.
+ *
+ * @returns {React.ReactElement} A React element representing the logout button with a tooltip.
+ */
 const LogOutButton = () => {
   const { logout } = useAuth();
   const handleLogout = () => {
@@ -65,6 +88,18 @@ const LogOutButton = () => {
   );
 };
 
+/**
+ * ChangeWorkspaceButton - Functional component to render a change workspace button.
+ *
+ * This component creates a button for changing the user's current workspace. Clicking the button
+ * navigates the user to a specified route ('/login/login_with_group') to handle workspace change.
+ * It also features a tooltip with the text "Change Workspace" on hover.
+ *
+ * Implements navigation using the 'useNavigate' hook from 'react-router-dom'. The button is created
+ * using Material UI's IconButton and Tooltip components, and the Diversity3Icon for the button's icon.
+ *
+ * @returns {React.ReactElement} A React element representing the change workspace button with a tooltip.
+ */
 const ChangeWorkspaceButton = () => {
   const navigate = useNavigate();
   const handleLogoutGroup = () => {
@@ -119,11 +154,21 @@ export function TopAppBar() {
 
   const [linksList, setLinksList] = useState([]);
 
+  /**
+   * getLinksList - Asynchronously fetches a list of hyperlinks for the current project.
+   *
+   * This function makes an HTTP GET request to retrieve all hyperlinks associated with the
+   * current project. It uses Axios for the request, with the project's title appended to the URL
+   * and JWT authorization in the header. The fetched data is then set to the linksList state.
+   *
+   * @async
+   * @function
+   * @throws Will log an error to the console if the request fails.
+   */
   const getLinksList = async () => {
     try {
       const response = await axios.get(
         `${ENDPOINT_URL}hyperlinks/getAllHyperlinks/${projectTitle}`,
-        //const response = await axios.get(`http://localhost:8080/getAllHyperlinks/${projectTitle}`,
         {
           headers: {
             Authorization: `Bearer ${auth.user.userJWT}`,
@@ -141,6 +186,19 @@ export function TopAppBar() {
     getLinksList();
   });
 
+  /**
+   * updateLinksList - Asynchronously updates a specific hyperlink in the list.
+   *
+   * This function sends an HTTP PUT request to update an existing hyperlink. It requires the edited
+   * link object and its unique identifier. The request is sent with JSON content-type and JWT
+   * authorization headers. The function is used to save edits made to a hyperlink.
+   *
+   * @async
+   * @function
+   * @param {Object} editedlink - The edited hyperlink object containing updated data.
+   * @param {string} thisid - The unique identifier of the hyperlink object to be updated.
+   * @throws Will log an error to the console if the update fails.
+   */
   async function updateLinksList(editedlink, thisid) {
     //e.preventDefault();
     // console.log("edited link", editedlink);
@@ -161,6 +219,18 @@ export function TopAppBar() {
     }
   }
 
+/**
+ * deleteLinksList - Asynchronously deletes a specific hyperlink from the list.
+ *
+ * This function performs an HTTP DELETE request to remove a hyperlink. It requires the unique
+ * identifier of the hyperlink to be deleted. The request is made with JSON content-type and
+ * JWT authorization headers.
+ *
+ * @async
+ * @function
+ * @param {string} thisid - The unique identifier of the hyperlink objrct to be deleted.
+ * @throws Will log an error to the console if the deletion fails.
+ */
   async function deleteLinksList(thisid) {
     try {
       await axios.delete(
@@ -177,6 +247,19 @@ export function TopAppBar() {
     }
   }
 
+/**
+ * submitLinksList - Asynchronously adds a new hyperlink to the list.
+ *
+ * This function is responsible for submitting a new hyperlink. It sends an HTTP POST request
+ * with the new link data. The project title is appended to the URL, and the request is sent with
+ * JSON content-type and JWT authorization headers. The response includes the ID of the new link,
+ * which is then appended to the link object.
+ *
+ * @async
+ * @function
+ * @param {Object} newlink - The new hyperlink object to be added.
+ * @throws Will log an error to the console if the submission fails.
+ */
   async function submitLinksList(newlink) {
     // e.preventDefault();
     try {
@@ -196,27 +279,51 @@ export function TopAppBar() {
     }
   }
 
-  const [anchorElUser, setAnchorElUser] = React.useState(null);
+  // State declarations for the component
+  const [anchorElUser, setAnchorElUser] = React.useState(null); // State for anchor element in user menu
   const settings = ["Option to be determined"];
 
-  const [state, setState] = useState(false); //drawer state
+  const [state, setState] = useState(false); // State to control the opening/closing of the hyperlinks drawer
+  const [action, setAction] = useState("Static"); // State to manage actions on adding a new hyperlink
+  const [itAction, setitAction] = useState("Static"); // State to manage actions on changing/removing hyperlinks list items
+  const [linkName, setLinkName] = useState(""); // State to store the name of the hyperlink
+  const [link, setLink] = useState(""); // State to store the URL of the hyperlink
 
-  const [action, setAction] = useState("Static"); //actions on add new hyperlink
-  const [itAction, setitAction] = useState("Static"); //actions on change/remove list items
-  const [linkName, setLinkName] = useState("");
-  const [link, setLink] = useState("");
 
+/**
+ * handleCloseUserMenu - Closes the user menu.
+ *
+ * This function resets the anchorElUser state to null, effectively closing user menu.
+ */
   const handleCloseUserMenu = () => {
     setAnchorElUser(null);
   };
 
+/**
+ * changeName - Updates the linkName state based on user input.
+ *
+ * @param {Object} event - The event object from the input field, contains the user inputted name for the link
+ */
   function changeName(event) {
     setLinkName(event.target.value);
   }
+
+/**
+ * changeLink - Updates the link URL state based on user input.
+ *
+ * @param {Object} event - The event object from the input field, contains the user inputted link
+ */
   function changeLink(event) {
     setLink(event.target.value);
   }
 
+/**
+ * addLinks - Adds a new hyperlink to the list.
+ *
+ * This function creates a new hyperlink object and adds it to the links list state. It also
+ * calls the submitLinksList function to handle the submission of the new link to the backend.
+ * It checks for the validity of the link name and URL before adding.
+ */
   function addLinks() {
     if (!linkName || !link) {
       return;
@@ -232,6 +339,15 @@ export function TopAppBar() {
     submitLinksList(newlink);
   }
 
+/**
+ * editLinks - Edits an existing hyperlink in the list.
+ *
+ * This function updates an existing hyperlink based on the given ID. It checks for the validity
+ * of the updated link name and URL before updating. It also updates the local state and calls
+ * updateLinksList to handle the backend update.
+ *
+ * @param {string} thisid - The unique ID of the hyperlink object to be edited.
+ */
   function editLinks(thisid) {
     if (!linkName || !link) {
       return;
@@ -249,6 +365,14 @@ export function TopAppBar() {
     updateLinksList(editedItem, thisid);
   }
 
+/**
+ * removeLinks - Removes a hyperlink from the list.
+ *
+ * This function deletes a hyperlink based on the given ID. It updates the local state and calls
+ * deleteLinksList to handle the removal in the backend.
+ *
+ * @param {string} thisid - The unique ID of the hyperlink object to be removed.
+ */
   function removeLinks(thisid) {
     const newlist = linksList.filter(
       (userlink) => userlink.hyperlinkId !== thisid
@@ -257,10 +381,22 @@ export function TopAppBar() {
     deleteLinksList(thisid);
   }
 
+/**
+ * toggleDrawer - Toggles the state of the drawer.
+ *
+ * This function is used to open or close the hyperlink management drawer based on the user's action.
+ *
+ * @param {boolean} event - The boolean value to set the drawer's open/close state.
+ */
   const toggleDrawer = (event) => {
     setState(event);
   };
 
+/**
+ * cancelAction - Resets the hyperlink action states.
+ *
+ * This function is used to reset the states related to hyperlink actions to their default values.
+ */
   function cancelAction() {
     setAction("Static");
     setitAction("Static");
@@ -268,6 +404,23 @@ export function TopAppBar() {
     setLinkName("");
   }
 
+/**
+ * list - A functional component that renders a list of hyperlink management options.
+ *
+ * This function returns a Material UI Box component containing a list of hyperlink-related actions.
+ * It includes an 'Add New Hyperlink' button and dynamically renders a list of existing hyperlinks,
+ * each with options to edit or remove. The component also provides input fields for adding or
+ * editing hyperlink names and URLs.
+ *
+ * The function uses the Material UI components such as Box, ListItemButton, TextField, and ButtonGroup
+ * for rendering the UI elements. It utilizes the component state such as 'action' and 'itAction' to
+ * determine the current action to be performed (add, edit, or remove hyperlinks).
+ *
+ * The list dynamically changes based on user interaction, allowing for real-time addition, edition,
+ * or deletion of hyperlinks in the list.
+ *
+ * @returns {React.ReactElement} A React element representing a list of hyperlink management options.
+ */
   const list = () => (
     <Box sx={{ width: "auto", marginTop: "70px" }} role="presentation">
       <ListItemButton
